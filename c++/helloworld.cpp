f71fbae9cfdbc6337275aa411ffde92d4da6b133
haseebahmedWD <haseebahmed23071991@gmail.com>
2024-03-21 06:32:44 +0500

    hello world

---
+++ c++/helloworld.cpp
@@ -0,0 +1,18 @@
+#include<sstream>
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+int main()
+{
+    string str = "";
+    int a;
+    cout<<"Please enter a string : ";
+    getline(cin, str);
+    cout<<str<<"\n";
+    cout<<"Please enter a number : ";
+    cin>>a;
+    cout<<"number is: "<<a<<"\n";
+    return 0;
+}
---
+++ c/helloworld.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int a;
+    printf("hello world \n");
+    printf("Enter a number ");
+    scanf("%d",&a);
+    printf("%d \n",a);
+    
+    return 0;
+}
