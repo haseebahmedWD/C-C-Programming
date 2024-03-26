41767e88059c27a4239e1977bd0157d452c3036c
haseebahmedWD <haseebahmed23071991@gmail.com>
2024-03-26 22:05:26 +0500

    hello world updated input file created

---
+++ c/input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    //program for adding two values
+    int a,b,sum;
+    printf("Enter two values : ");
+    scanf("%d %d",&a,&b);
+    sum = a+b;
+    printf("sum of values %d and %d is : %d \n",a,b,sum);
+    return 0;
+}
