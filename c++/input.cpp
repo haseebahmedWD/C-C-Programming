5412dfeddf385a37b1efc5bd2f55c3c1fb5a8484
haseebahmedWD <haseebahmed23071991@gmail.com>
2024-04-04 09:46:08 +0500

    variables tutorials

---
+++ c++/input.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int inpt;
+    string str;
+    cout<<"Please enter a number: ";
+    cin>>inpt;
+    cout<<"Number is: "<<inpt<<"\n";
+    getchar();
+    cout<<"Please enter sentence: ";
+    getline(cin,str);  //we do not use cin for string input because cin will consider whitespace as termination character and it will print only one word and remaining words array will be move to next line
+    cout<<"Sentence is: "<<str<<endl;
+
+    cout<<"Please enter a word: ";
+    cin>>str;
+    cout<<"Sentence is: "<<str<<"\n";
+    
+   
+    return 0;
+}
---
+++ c++/variables.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int myNum = 5, myNum2 = 6;
+    double mydoubleNum = 12.57;
+    char myChar = 'A';
+    float myfloatNum = 6.77;
+    string myText = "Hello World";
+    bool myBoolean = false;
+    const float pi = 3.14; //const value will not be changed
+   // pi = 45.6; //error
+
+    cout<<"Integer Number: "<<myNum<<endl;
+    cout<<"Double Number: "<<mydoubleNum<<endl;
+    cout<<"Character: "<<myChar<<endl;
+    cout<<"Float Number: "<<myfloatNum<<endl;
+    cout<<"String: "<<myText<<endl;
+    cout<<"Boolean: "<<myBoolean<<endl;
+    cout<<"Sum of "<<myNum<<" and "<<myNum2<<" is : "<<myNum + myNum2<<endl;
+    cout<<"Constant variable : "<<pi<<endl;
+    return 0;
+}
