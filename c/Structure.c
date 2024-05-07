8656ec3198bf28ae3ea11014959cfe153e8de4e9
haseebahmedWD <haseebahmed23071991@gmail.com>
2024-05-07 22:17:29 +0500

    Added input output of structure array

--- c/Structure.c
+++ c/Structure.c
@@ -16,14 +16,29 @@ struct date inputDate()
 void outputDate(struct date temp)
 {
     printf("%d-%d-%d \n",temp.day,temp.month,temp.year);
-    //this method is if you want to output date as 23/08/1991 format. We use / as delimiter here
+    //this method is if you want to output date as 23-08-1991 format. We use - as delimiter here
     // printf("%d/%d/%d",&temp.day, &temp.month, &temp.year);
 }
 int main()
 {
     struct date d1;
+    int size = 4;
+    struct date dob[size];
+    //input and output of single date
     d1 = inputDate();
     outputDate(d1);
+
+    //input in date type array
+    for (int i = 0; i < size; i++)
+    {
+        dob[i] = inputDate();
+    }
+    //output of date type array
+    for (int i = 0; i < size; i++)
+    {
+        outputDate(dob[i]);
+    }
+    
 
     return 0;
 }
